ceb508b7aab90519e0093efad3c0e907cecf4da9
= <thomaschriskelly@gmail.com>
2017-10-24 16:23:29 -0400

    movie table added. dummy data

--- spa/react-app/src/App.js
+++ spa/react-app/src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const djangoServer = 'http://localhost:8000';
+const actorsUrl = djangoServer + '/actors/';
+const moviesUrl = djangoServer + '/movies/';
+
+function Movie(props) {
+    return(
+        <tr>
+            <td>title</td>
+            <td>genre</td>
+        </tr>
+    )
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Movie Tracker</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    renderMovie() {
+        return <Movie/>;
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Movie Tracker</h1>
+                </header>
+                <br/>
+                <table>
+                    <tr>
+                        <th>Title</th>
+                        <th>Genre</th>
+                    </tr>
+                    {this.renderMovie()}
+                    {this.renderMovie()}
+                    {this.renderMovie()}
+                    {this.renderMovie()}
+                </table>
+            </div>
+        );
+    }
 }
 
 export default App;
